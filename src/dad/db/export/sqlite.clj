5c809be8522acb07e8dae01f5994da8eea2d4b9b
Avi Flax <avi.flax@fundingcircle.com>
2020-06-09 13:54:08 -0400

    START: New feature: generate SQLite DDL to create tables
    For exporting a DaD DB to SQLite.
    
    Refs #25
    Refs #22

---
+++ src/dad/db/export/sqlite.clj
@@ -0,0 +1,27 @@
+(ns dad.db.export.sqlite
+  (:require ;[clojure.spec.alpha :as s]
+            ;[cognitect.anomalies :as anom]
+            [dad.db.export.tables :as et]
+            ;[medley.core :as mc :refer [map-vals]]
+            [next.jdbc :as j]
+            [next.jdbc.sql :as sql]))
+
+(defn- create-table
+  [name rows ds]
+  nil)
+
+(defn- insert-rows
+  [rs-name rows ds]
+  nil)
+
+(defn table->ddl
+  "Accepts a ::dad.db.export.tables/tables, returns a ::dad.db.export.sql/ddl-statement"
+  [table-name rows]
+  nil)
+
+(defn db->sqlite
+  "On success, returns true."
+  [flattened-dad-db db-out-path]
+  (let [out-db-spec {:dbtype "sqlite" :dbname db-out-path}
+        out-ds (j/get-datasource out-db-spec)]
+    :TODO))
---
+++ test/examples/dad/db/export/sqlite_test.clj
@@ -0,0 +1,26 @@
+(ns dad.db.export.sqlite-test
+  (:require [clojure.test :refer [deftest is are testing]]
+            [dad.db.export.tables :as et]
+            [dad.db.export.sqlite :as es]))
+
+; TODO
+; (deftest table->ddl
+;   (are [expected table-name table-rows] (= expected (#'es/table->ddl table-name table-rows))
+;
+;     :technologies
+;     {"Clojure" {"links-main" "https://clojure.org/"}
+;      "JRE"     {"summary" "A runtime environment for Java programs."}}
+;     {:create-table :technologies
+;      :columns ["name varchar(255) not null primary key"
+;                "links-main text"
+;                "summary text"]}
+;
+;     :technologies-recommendations
+;     [(with-meta {:technology "Clojure", "type" "assess", "date" "2011-09-15"}
+;                 {::et/columns {:technology {::et/fk-table-name :technologies}}})
+;      (with-meta {:technology "Clojure", "type" "adopt", "date" "2012-01-12"}
+;                 {::et/columns {:technology {::et/fk-table-name :technologies}}})]
+;     {:create-table :technologies-recommendations
+;      :columns ["technology varchar(255) not null references technologies"
+;                "type text"
+;                "date text"]}))
