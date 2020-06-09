74de883f43f71216b4a6a9d07a87702fbe433902
Avi Flax <avi.flax@fundingcircle.com>
2020-06-09 13:54:08 -0400

    START: New feature: flatten a DB into generic tables
    For exporting a DaD DB to a tabular database or a spreadsheet.
    
    Refs #24
    Refs #22

---
+++ src/dad/db/export/tables.clj
@@ -0,0 +1,185 @@
+(ns dad.db.export.tables
+  (:require [clojure.string :as str]
+            [clojure.spec.alpha :as s]
+            [clojure.walk :as walk :refer [postwalk]]
+            [dad.medley :as dm]
+            [inflections.core :refer [singular]]
+            [medley.core :as mc :refer [map-keys map-vals]]))
+
+;; Maybe should this be a library, something like flattt -> Flatten To Tables — ?
+;; Prior art: https://github.com/OpenDataServices/flatten-tool
+
+(s/def ::non-blank-string (s/and string? (complement str/blank?)))
+(s/def ::non-empty-scalar (s/or :number number?
+                                :keyword keyword?
+                                :string ::non-blank-string))
+(s/def ::col-name         keyword?)
+(s/def ::col-val          (s/or :scalar      ::non-empty-scalar
+                                :scalar-coll (s/coll-of ::non-empty-scalar :gen-max 10)))
+(s/def ::record-key-cols  (s/map-of ::col-name ::col-val :gen-max 10))
+(s/def ::record-val-cols  (s/map-of ::col-name ::col-val :gen-max 10))
+(s/def ::keyed-rows       (s/map-of ::record-key-cols ::record-val-cols :gen-max 10))
+(s/def ::unkeyed-rows     (s/coll-of ::record-val-cols :gen-max 10))
+(s/def ::table-name       keyword?)
+(s/def ::tables           (s/map-of ::table-name
+                                    (s/or :keyed ::keyed-rows
+                                          :unkeyed ::unkeyed-rows)
+                                    :gen-max 10))
+
+(defn- join-names
+  [separator names]
+  (->> (map name names)
+       (str/join separator)
+       (keyword)))
+
+; TODO: not currently working
+; (s/def ::simple-map
+;   (s/map-of (s/or :s string? :k keyword?)
+;             (s/or :s ::non-empty-scalar
+;                   :sc (s/coll-of ::non-empty-scalar :gen-max 10)
+;                   :m ::simple-map)
+;             :gen-max 10))
+;
+; (s/fdef fold-props
+;   :args (s/cat :m (s/with-gen ::simple-map
+;                     #(gen/frequency [[9 (s/gen ::simple-map)] [1 (gen/return {:foo {:bar :baz :props {:blargh :flargh}}})]])))
+;   :ret  ::simple-map
+;   :fn   (fn [{{in :m} :args
+;               out     :ret}]
+;           (let [map-seq (fn map-seq [m] (tree-seq map? #(interleave (keys %) (vals %)) m))]
+;             (if (some :props (map-seq (s/unform ::simple-map in)))
+;               (not-any? :props (map-seq (s/unform ::simple-map out)))
+;               (= out in)))))
+
+(defn- fold-props
+  [m]
+  (postwalk
+    (fn [v]
+      (if-let [props (and (map? v) (map? (:props v)) (:props v))]
+        (merge props (dissoc v :props))
+        v))
+    m))
+
+(defn- flatten-paths
+  {:derived-from "https://andersmurphy.com/2019/11/30/clojure-flattening-key-paths.html"}
+  ([m separator]
+   (flatten-paths m separator []))
+  ([m separator path]
+   (->> (map (fn [[k v]]
+               (if (and (map? v)
+                        (not-empty v)
+                        (not (map? (val (first v)))))
+                 (flatten-paths v separator (conj path k))
+                 [(->> (conj path k)
+                       (join-names separator))
+                  (if (and (map? v)
+                           (some-> v first val map?))
+                    (flatten-paths v separator)
+                    v)]))
+             m)
+        (into {}))))
+
+(defn- ->key-col
+  [k]
+  (cond
+    (map? k) k
+    (or (string? k) (keyword? k)) {:name (name k)}))
+
+(s/fdef add-fk
+  :args (s/cat :val-cols         ::record-val-cols
+               :fk-table-name    ::table-name
+               :fk-table-key-val ::col-val)
+  :ret  ::record-val-cols)
+
+(defn- add-fk
+  [rec-m fk-table-name fk-table-key-val]
+  (let [col-name (singular fk-table-name)
+        key-val (if (keyword? fk-table-key-val)
+                  (name fk-table-key-val)
+                  fk-table-key-val)]
+    (-> (assoc rec-m col-name key-val)
+        (with-meta {::columns {col-name {::fk-table-name fk-table-name}}}))))
+
+(def separator "-")
+
+(defn- split-record
+  "Accepts a table name and a single record as either a MapEntry or a two-tuple.
+  Returns a map of table name to maps representing records."
+  [table-name [rec-key rec-m :as _record]]
+  (reduce-kv
+    (fn [r k v]
+      (cond
+        (and (coll? v) (map? v))
+        (assoc r
+              (join-names separator [table-name k])
+              (map-keys #(-> (->key-col %)
+                             (add-fk table-name rec-key)) v))
+        
+        (and (coll? v)
+             (not (map? v))
+             (map? (first v)))
+        (assoc r
+              (join-names separator [table-name k])
+              (map #(add-fk % table-name rec-key) v))
+        
+        :else
+        (assoc-in r [table-name (->key-col rec-key) k] v)))
+    {}
+    rec-m))
+
+(defn recordset->tables
+  "Transforms the recordset into one or more tables. The recordset should be either a MapEntry or a
+  two-tuple. Returns a map."
+  [[rs-name rs-recs :as _recordset]]
+  (->> (walk/keywordize-keys rs-recs)
+       (map-vals fold-props)
+       (map-vals #(flatten-paths % separator))
+       (map #(split-record rs-name %))
+       (reduce dm/deep-merge)))
+
+(defn flatten-db
+  [db]
+  (->> (dissoc db :schemata)
+       (pmap recordset->tables)
+       (reduce merge)))
+
+
+
+
+
+(comment
+  (require '[dad.db :as db] '[clojure.pprint :refer [pprint]])
+  (def db-path "/Users/avi.flax/dev/docs/architecture/docs-as-data/db")
+  (def db (db/read db-path))
+  (defn rand-val [m] (-> m seq rand-nth val))
+  
+  (-> (select-keys db [:technologies])
+      (update :technologies #(select-keys % ["Clojure" "Kafka"]))
+      (find :technologies)
+      recordset->tables)
+      
+  (->  (select-keys db [:repositories]) :repositories count)
+
+  (->> (mc/map-entry :technologies {"Clojure" {"links" {"main" "https://clojure.org/"}
+                                              "recommendations" [{"type" "assess", "date" "2011-09-15"}
+                                                                 {"type" "adopt", "date" "2012-01-12"}]}})
+       recordset->tables)
+  
+  (-> (select-keys db [:technologies])
+      (update :technologies #(select-keys % ["Clojure"]))
+      (find :technologies)
+      recordset->tables
+      :technologies-recommendations
+      first
+      meta)
+      
+  (-> (find db :systems)
+      (recordset->tables))
+
+  (-> (map-vals #(into {} (take 2 %)) db)
+      (flatten-db)
+      (pprint))
+  
+  (flatten-db db)
+  
+  (s/exercise ::tables))
---
+++ test/examples/dad/db/export/tables_test.clj
@@ -0,0 +1,167 @@
+(ns dad.db.export.tables-test
+  (:require [clojure.spec.alpha :as s]
+            [clojure.test :refer [deftest is are]]
+            [dad.db.export.tables :as et]
+            [expound.alpha :as expound]
+            [medley.core :as mc :refer [map-entry]]))
+
+; See https://github.com/bhb/expound#printer-options
+; (set! s/*explain-out* (expound/custom-printer {:print-specs? false :theme :figwheel-theme}))
+
+; TODO: not currently working
+; (st/instrument `et/add-fk `et/fold-props)
+
+(deftest add-fk
+  (let [rec-m {:type "assess"
+               :date "2011-09-15"}
+        fk-table-name :technologies
+        fk-table-key-val "Clojure"
+        expected {:technology "Clojure"
+                  :type "assess"
+                  :date "2011-09-15"}
+        expected-meta {::et/columns {:technology {::et/fk-table-name fk-table-name}}}
+        res (#'et/add-fk rec-m fk-table-name fk-table-key-val)]
+    (is (= expected res))
+    (is (= expected-meta (meta res)))))
+
+(deftest fold-props
+  (are [in expected] (= expected (#'et/fold-props in))
+  
+    ; in
+    {:containers  {:API     {:props {:marathon-ids {:kp "/saclib/api"}}}
+                   :Hutch   {:props {:marathon-ids {:kp "/saclib/hutch"}
+                                     :technologies ["RabbitMQ" "Ruby"]}}
+                   :Sidekiq {:props {:marathon-ids {:kp "/saclib/sidekiq"}
+                                     :technologies ["Ruby"]}}
+                   :Web     {:props {:marathon-ids {:kp "/saclib/web"}}}}
+     :description "Salad Container Library -- builds libraries of salad containers (duh)"
+     :props       {:regions       ["kp"]
+                   :marathon-ids  {:kp "/saclib"}
+                   :repos         ["saclib"]
+                   :related-repos ["saclib_adapter" "saclib-client"]}}
+    ; expected
+    {:containers   {:API     {:marathon-ids {:kp "/saclib/api"}}
+                    :Hutch   {:marathon-ids {:kp "/saclib/hutch"}
+                              :technologies ["RabbitMQ" "Ruby"]}
+                    :Sidekiq {:marathon-ids {:kp "/saclib/sidekiq"}
+                              :technologies ["Ruby"]}
+                    :Web     {:marathon-ids {:kp "/saclib/web"}}}
+     :description   "Salad Container Library -- builds libraries of salad containers (duh)"
+     :regions       ["kp"]
+     :marathon-ids  {:kp "/saclib"}
+     :repos         ["saclib"]
+     :related-repos ["saclib_adapter" "saclib-client"]}))
+
+(deftest flatten-paths
+  (are [in expected] (= expected (#'et/flatten-paths in "-"))
+  
+    ; in
+    {:containers    {:API     {:marathon-ids {:kp "/saclib/api"}}
+                     :Hutch   {:marathon-ids {:kp "/saclib/hutch"}
+                               :technologies ["RabbitMQ" "Ruby"]}
+                     :Sidekiq {:marathon-ids {:kp "/saclib/sidekiq"}
+                               :technologies ["Ruby"]}
+                     :Web     {:marathon-ids {:kp "/saclib/web"}}}
+     :description   "Salad Container Library -- builds libraries of salad containers (duh)"
+     :regions       ["kp"]
+     :marathon-ids  {:kp "/saclib"}
+     :repos         ["saclib"]
+     :related-repos ["saclib_adapter" "saclib-client"]}
+
+    ; expected
+    {:containers      {:API     {:marathon-ids-kp "/saclib/api"}
+                       :Hutch   {:marathon-ids-kp "/saclib/hutch"
+                                 :technologies    ["RabbitMQ" "Ruby"]}
+                       :Sidekiq {:marathon-ids-kp "/saclib/sidekiq"
+                                 :technologies    ["Ruby"]}
+                       :Web     {:marathon-ids-kp "/saclib/web"}}
+     :description     "Salad Container Library -- builds libraries of salad containers (duh)"
+     :regions         ["kp"]
+     :marathon-ids-kp "/saclib"
+     :repos           ["saclib"]
+     :related-repos   ["saclib_adapter" "saclib-client"]}))
+
+(deftest split-record
+  (are [table-name record expected] (= expected (#'et/split-record table-name record))
+    ; table-name
+    :technologies
+    ; record
+    (map-entry "Clojure" {:links-main "https://clojure.org/"
+                          :recommendations [{:type "assess" :date "2011-09-15"}
+                                            {:type "adopt"  :date "2012-01-12"}]})
+    ; expected
+    {:technologies                 {{:name "Clojure"} {:links-main "https://clojure.org/"}}
+     :technologies-recommendations [{:technology "Clojure" :type "assess" :date "2011-09-15"}
+                                    {:technology "Clojure" :type "adopt"  :date "2012-01-12"}]}
+
+    ; --------------------
+
+    ; table-name
+    :systems
+    ; record
+    (map-entry "Discourse" {:links-main "https://discourse.org/"
+                            :containers {:web   {:summary "web server" :technology "Tomcat"}
+                                         :db    {:summary "db server"  :technology "Access"}
+                                         :cache {:summary "hot keys"   :technology "PHP"}}})
+    ; expected
+    {:systems            {{:name "Discourse"} {:links-main "https://discourse.org/"}}
+     :systems-containers {{:system "Discourse" :name "web"}   {:summary "web server" :technology "Tomcat"}
+                          {:system "Discourse" :name "db"}    {:summary "db server"  :technology "Access"}
+                          {:system "Discourse" :name "cache"} {:summary "hot keys"   :technology "PHP"}}}
+
+    ; --------------------
+    
+    ; table-name
+    :systems
+    ; record
+    (map-entry "SACLIB" {:containers      {:API     {:marathon-ids-kp "/saclib/api"}
+                                           :Hutch   {:marathon-ids-kp "/saclib/hutch"
+                                                     :technologies ["RabbitMQ" "Ruby"]}
+                                           :Sidekiq {:marathon-ids-kp "/saclib/sidekiq"
+                                                     :technologies ["Ruby"]}
+                                           :Web     {:marathon-ids-kp "/saclib/web"}}
+                         :description     "Salad Container Library -- builds libraries of salad containers (duh)"
+                         :regions         ["kp"]
+                         :marathon-ids-kp "/saclib"
+                         :repos           ["saclib"]
+                         :related-repos   ["saclib_adapter" "saclib-client"]})
+    ; expected
+    {:systems            {{:name "SACLIB"} {:description     "Salad Container Library -- builds libraries of salad containers (duh)"
+                                            :regions         ["kp"]
+                                            :marathon-ids-kp "/saclib"
+                                            :repos           ["saclib"]
+                                            :related-repos   ["saclib_adapter" "saclib-client"]}}
+     :systems-containers {{:system "SACLIB" :name "API"}     {:marathon-ids-kp "/saclib/api"}
+                          {:system "SACLIB" :name "Hutch"}   {:marathon-ids-kp "/saclib/hutch"
+                                                             :technologies    ["RabbitMQ" "Ruby"]}
+                          {:system "SACLIB" :name "Sidekiq"} {:marathon-ids-kp "/saclib/sidekiq"
+                                                             :technologies    ["Ruby"]}
+                          {:system "SACLIB" :name "Web"}     {:marathon-ids-kp "/saclib/web"}}})
+
+  (let [table-name :technologies
+        record (map-entry "Clojure" {:links-main "https://clojure.org/"
+                                     :recommendations [{:type "assess" :date "2011-09-15"}
+                                                       {:type "adopt"  :date "2012-01-12"}]})
+        res (#'et/split-record table-name record)]
+    (is (s/valid? ::et/tables res) (s/explain-str ::et/tables res))))
+
+(deftest recordset->tables
+  (let [recordset (map-entry :technologies {"Clojure" {"links" {"main" "https://clojure.org/"}
+                                                       "props" {"hosted" "true"}
+                                                       "recommendations" [{"type" "assess", "date" "2011-09-15"}
+                                                                          {"type" "adopt", "date" "2012-01-12"}]}
+                                            "Kafka"   {"links" {"main" "https://kafka.apache.org/"}
+                                                       "recommendations" [{"type" "assess", "date" "2013-12-16"}
+                                                                          {"type" "adopt", "date" "2016-03-03"}]}})
+        expected {:technologies {{:name "Clojure"} {:links-main "https://clojure.org/"
+                                                    :hosted     "true"}
+                                 {:name "Kafka"}   {:links-main "https://kafka.apache.org/"}}
+                  :technologies-recommendations [{:technology "Clojure" :type "assess" :date "2011-09-15"}
+                                                 {:technology "Clojure" :type "adopt"  :date "2012-01-12"}
+                                                 {:technology "Kafka" :type "assess" :date "2013-12-16"}
+                                                 {:technology "Kafka" :type "adopt"  :date "2016-03-03"}]}
+        res (#'et/recordset->tables recordset)]
+  (is (= expected res))
+  (is (s/valid? ::et/tables res) (s/explain-str ::et/tables res))
+  (is (= {::et/columns {:technology {::et/fk-table-name :technologies}}}
+         (meta (first (:technologies-recommendations res)))))))
