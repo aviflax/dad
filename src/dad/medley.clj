48cf7c8c4bd09c0c54401a9b3e2498e407b87ea2
Avi Flax <avi.flax@fundingcircle.com>
2020-06-15 11:14:01 -0400

    TSQD rename fn

--- src/dad/medley.clj
+++ src/dad/medley.clj
@@ -16,8 +16,7 @@
   * If the two values under the same keys are both collections, they’re concatenated
   * Otherwise, the value is overwritten as in `clojure.core/merge`
   
-  This is a “slightly” modified version of `medley.core/deep-merge` from
-  https://github.com/weavejester/medley"
+  This is a modified version of `medley.core/deep-merge` from https://github.com/weavejester/medley"
   {:arglists '([& maps])
    :based-on "https://github.com/weavejester/medley/blob/6c79c4cce52b276daa3c2b6eaea78f96904bca56/src/medley/core.cljc#L213"}
   ([])
@@ -29,18 +28,11 @@
                      v' (val e)]
                  (if (contains? m k)
                    (assoc m k (let [v (get m k)]
-                                (cond
-                                  (and (map? v) (map? v'))
-                                  (deep-merge v v')
-                                  
-                                  (and (set? v) (set? v'))
-                                  (union v v')
-                                  
-                                  (and (coll? v) (coll? v'))
-                                  (concat v v')
-                                  
-                                  :else v')))
+                                (cond (and (map? v) (map? v'))    (deep-mergecat v v')
+                                      (and (set? v) (set? v'))    (union v v')
+                                      (and (coll? v) (coll? v'))  (concat v v')
+                                      :else                       v')))
                    (assoc m k v'))))]
        (reduce merge-entry (or a {}) (seq b)))))
   ([a b & more]
-   (reduce deep-merge (or a {}) (cons b more))))
+   (reduce deep-mergecat (or a {}) (cons b more))))
